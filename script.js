73555a11ce0e429ae60226a931b03a9f840c9fed
888Saint <psdas32@gmail.com>
2025-01-02 11:00:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
-    //Write your code here
-
+    // Prevent the form from submitting and refreshing the page
+    event.preventDefault();
+    
+    // Get the form element by its ID
+    const form = document.getElementById("form1");
+    
+    // Access the input fields using their `name` attributes
+    const firstName = form.elements["fname"].value;
+    const lastName = form.elements["lname"].value;
+    
+    // Alert the full name
+    alert(firstName + " " + lastName);
 }
